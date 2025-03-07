28a05a47ecf6f718eba2211d6150849e1c4b69f2
kknirala <kk.nirala@gmail>
2025-03-07 09:33:54 +0530

    Added public IP

--- resources.tf
+++ resources.tf
@@ -19,4 +19,22 @@ resource "azurerm_subnet" "sub" {
   address_prefixes     = ["10.0.1.0/24"]
 }
 
+resource "azurerm_public_ip" "pip" {
+  name                = "my-pip"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  allocation_method   = "Static"
+}
 
+resource "azurerm_network_interface" "linux-nic" {
+  name                = "linux-nic"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.sub.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id = azurerm_public_ip.pip.id
+  }
+}
