ec09e735dd6377791eb1f971596fdf8ff82a103a
kknirala <kk.nirala@gmail>
2025-03-07 09:37:36 +0530

    Added linux vm

--- resources.tf
+++ resources.tf
@@ -38,3 +38,28 @@ resource "azurerm_network_interface" "linux-nic" {
     public_ip_address_id = azurerm_public_ip.pip.id
   }
 }
+
+resource "azurerm_linux_virtual_machine" "vm" {
+  name                = "pranav-linux"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  size                = "Standard_F2"
+  admin_username      = "pranav"
+  admin_password = "Ubuntu@123"
+  disable_password_authentication = false
+  network_interface_ids = [
+    azurerm_network_interface.linux-nic.id,
+  ]
+
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Standard_LRS"  
+  }
+
+  source_image_reference {
+    publisher = "canonical"
+    offer     = "0001-com-ubuntu-server-jammy"
+    sku       = "22_04-lts"
+    version   = "latest"
+  }
+}
